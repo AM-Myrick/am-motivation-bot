3a415675fb4de407c88549e268fc6106fcdca65e
AM_Myrick <amyrick.820@gmail.com>
2018-11-18 17:59:16 -0500

    adds quote posting functionality

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Twit = require("twit");
 const TwitterBot = require("node-twitterbot").TwitterBot;
+const axios = require("axios");
+const url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
 
 const Bot = new TwitterBot({
  consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -8,21 +10,43 @@ const Bot = new TwitterBot({
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-const phraseArray = [ "hey twitter",
-                    "im tweeting",
-                    "tweet tweet",
-                    "tweetstorm time... 1/22",
-                    "plz RT v important",
-                    "delete ur account",
-                    "it me",
-                    "same",
-                    "#dogpants go on 4 legs!!",
-                    "#thedress is blue and black" ];
-
-function chooseRandom(myArray) {
-  return myArray[Math.floor(Math.random() * myArray.length)];
+const newQuote = () => {
+    // function to grab a quote from the api and pass it to be checked
+    axios.get(url)
+        .then(res => quoteChecker(res.data))
+        .catch(err => console.log(err))
 }
 
-let phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
+const quoteChecker = (quote) => {
+    // function checks if quote exists and formats it
+    if (quote === undefined) {
+        newQuote();
+    }
+    // replaces unicode characters
+    let quoteText = quote.quoteText.replace(/\\u[\dA-F]{4}/gi, 
+        function (match) {
+             return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+        })
+      
+    let quoteAuthor = quote.quoteAuthor.replace(/\\u[\dA-F]{4}/gi, 
+        function (match) {
+             return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+        })
+    
+    let fullQuote = `${quoteText}
+     -${quoteAuthor}
+#Motivation #KeepGoing`;
+    
+    // makes sure incomplete tweets aren't posted
+    if (fullQuote.length >= 280) {
+        newQuote();
+    } else {
+        tweetQuote(fullQuote);
+    }
+}
 
-Bot.tweet(phrase);
+const tweetQuote = (quote) => {
+    Bot.tweet(quote);
+}
+
+newQuote();
