c35778bf1d7faf89bd5b1e242b027911e25ccbeb
AM-Myrick <amyrick.820@gmail.com>
2019-11-10 10:07:48 -0500

    adds auto-retweeting when mentioned

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	listen := parseFlags()
+	if listen == true {
+		listenForMentions()
+	}
+}
+
+func parseFlags() bool {
+	listen := flag.Bool("listen", true, "a flag to make the bot listen for mentions of its handle")
+	flag.Parse()
+
+	return *listen
+}
+
+func listenForMentions() {
+	client := createTwitterClient()
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		if tweet.InReplyToStatusID != 0 {
+			retweetTweet(tweet.InReplyToStatusID, client)
+		}
+	}
+
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"@AMMotivationBo2"},
+		StallWarnings: twitter.Bool(true),
+	}
+
+	stream, err := client.Streams.Filter(params)
+
+	if err != nil {
+		log.Fatal("Failed to open stream.")
+	}
+
+	demux.HandleChan(stream.Messages)
+}
+
+func createTwitterClient() *twitter.Client {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	consumerKey := os.Getenv("BOT_CONSUMER_KEY")
+	consumerSecret := os.Getenv("BOT_CONSUMER_SECRET")
+	accessToken := os.Getenv("BOT_ACCESS_TOKEN")
+	accessSecret := os.Getenv("BOT_ACCESS_TOKEN_SECRET")
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+	return client
+}
+
+func retweetTweet(tweetID int64, client *twitter.Client) {
+	client.Statuses.Retweet(tweetID, nil)
+}
